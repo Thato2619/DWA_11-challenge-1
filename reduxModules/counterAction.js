b26d64b3b3d4d69df55b84c3aedd451c43199e43
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-05-11 15:43:44 +0200

    create a reduxModule folder that has a counterAction and countReducer file
    - in countAction.js file add the export function of the minus, add and rest functions

---
+++ reduxModules/counterAction.js
@@ -0,0 +1,20 @@
+export function Minus() {
+    return {
+        type: "MINUS"
+
+    }
+}
+
+export function Add() {
+    return {
+        type: "ADD"
+
+    }
+}
+
+export function Reset() {
+    return {
+        type: "RESET"
+
+    }
+}
